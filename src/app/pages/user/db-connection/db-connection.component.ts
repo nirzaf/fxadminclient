136fc199a6cbeec9dfa0608eda16ce78f7320c9d
Sirojan Gnanaretnam <sirojan.gnanaretnam@idsnext.com>
2021-03-26 19:12:23 +0530

    db config

--- src/app/pages/user/db-connection/db-connection.component.ts
+++ src/app/pages/user/db-connection/db-connection.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TRANSLATE } from 'src/app/shared/constant/translate';
+import { ToastService } from 'src/app/shared/services/toast.service';
+import { WebService } from 'src/app/shared/services/web.service';
 export interface PeriodicElement {
   name: string;
   productName:string;
@@ -31,15 +34,69 @@ export class DbConnectionComponent implements OnInit {
   TRANSLATE: any = TRANSLATE['en'];
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
-  public breadCrumb: string[] = [];
- 
-  
-  constructor(){}
+  holdingCompanyList = [];
+  groupList=[];
+  propertyList=[];
+  productData = [];
 
-  ngOnInit() {
-    this.breadCrumb = ["Admin", "DB Connection Access"];
+  public breadCrumb: string[] = [];
+  dbConfigForm: FormGroup = this.formBuilder.group({
+  'products': this.formBuilder.array([]),
+    holdingCompany: [, { validators: [Validators.required], updateOn: "change" }],
+    group: [, { validators: [Validators.required], updateOn: "change" }],
+    property: [, { validators: [Validators.required], updateOn: "change" }]
+    
+  });
+  
+  constructor(private formBuilder: FormBuilder,private webService: WebService, private toast: ToastService){
+
+
+
+  }
+  addProduct(product?: any,isOpen?: any) {
+    let fg = this.formBuilder.group({
+      'live': ['', Validators.compose([Validators.required])],
+      'username': ['', Validators.compose([Validators.required])],
+      'password': ['', Validators.compose([Validators.required])],
+      'dbName': ['', Validators.compose([Validators.required])],
+      'productName': [product? product.name:'', Validators.compose([Validators.required])],
+     
+    });
+    (<FormArray>this.dbConfigForm.get('products')).push(fg);
+   
+   
   }
 
+  ngOnInit() {
+    this.getProductList();
+    this.breadCrumb = ["Admin", "DB Connection Access"];
+    this.getHoldingCompanyList();
+
+  }
+  getProductList() {
+  
+    this.webService.commonMethod('product/get?pageSize=1000', null, 'GET', null)
+      .subscribe(data => {
+        if (data.succeeded) {
+        this.productData = data.data;
+        if (this.productData) {
+          this.productData.forEach(product => {
+            this.addProduct(product);
+          
+          });
+        } else {
+          //this.addProperty(null,true);
+         
+       }
+        }
+        else {
+          this.productData = [];
+          this.toast.error(data.errors);
+        }
+  
+  
+      });
+  }
   onDelete(traineeId: string)
   {
     
@@ -48,4 +105,51 @@ export class DbConnectionComponent implements OnInit {
   ngOnDestroy() {
 
   }
+  getPropertyList(groupID) {
+
+    this.webService.commonMethod('property/getbygroup/' + groupID, null, 'GET', null)
+      .subscribe(data => {
+        if (data.succeeded) {
+          this.propertyList = data.data;
+        }
+        else {
+          this.propertyList = [];
+          this.toast.error(data.errors);
+        }
+
+
+      });
+  }
+  getHoldingCompanyList() {
+    this.webService.commonMethod('holdingcompany/get?pageSize=1000', null, 'GET', null)
+      .subscribe(data => {
+        if (data.succeeded) {         
+         
+         this.holdingCompanyList=data.data;
+        }
+        else {
+          this.toast.error(data.errors);
+        }
+
+
+
+      });
+  }
+  getCompanyGroupList(holdingCompanyID) {
+    this.webService.commonMethod('group/getbycompany/' + holdingCompanyID, null, 'GET', null)
+      .subscribe(data => {
+        if (data.succeeded) {
+          this.groupList = data.data;
+
+       
+
+
+
+        } else {
+          this.toast.error(data.errors);
+        }
+
+      });
+  }
+  onSubmit(formValue) {}
 }
